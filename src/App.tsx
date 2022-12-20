921cc6dd64a390f1ce541d66a1aea44834eb01f8
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-12-20 12:05:00 -0400

    Added Component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from 'styles/themes/default'
+import { Transactions } from 'pages/Transactions'
 import { GlobalStyle } from 'styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Transactions />
       <GlobalStyle />
     </ThemeProvider>
   )
